72208720b057fc5788de7b293920551c9fd987fc
tomik148 <tchalupa1997@gmail.com>
2018-10-29 00:05:24 +0100

    intit map character movment

---
+++ Assets/Scripts/controler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class controler : MonoBehaviour {
+
+    public Animator animator;
+    public Joystick joystick;
+    public Rigidbody2D rigidbody;
+
+    public float speed = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var dx = joystick.m_HorizontalVirtualAxis.GetValue;
+        var dy = joystick.m_VerticalVirtualAxis.GetValue;
+        var delta = new Vector2();
+        if (dx > 0.1 || dx < -0.1)
+        {
+            animator.SetFloat("Xvel",dx);
+            delta.x += dx;
+        }
+        if (dy > 0.1 || dy < -0.1)
+        {
+            animator.SetFloat("Yvel",dy);
+            delta.y += dy;
+        }
+        delta *= Time.deltaTime;
+        delta *= speed;
+        rigidbody.MovePosition(rigidbody.position + delta);
+        
+	}
+}
